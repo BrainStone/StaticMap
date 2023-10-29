efb16dda0fc2a6e55a0b1df5f9491fda94e430da
BrainStone <yannick@tedworld.de>
2023-10-29 19:35:47 +0100

    Implement a variant that accepts totally_ordered and equality_comparable types
    The ordered ones being sorted on creation making the lookup fast.
    The other ones fall back to a linear search

--- src/static_map.hpp
+++ src/static_map.hpp
@@ -2,19 +2,89 @@
 #include <array>
 #include <concepts>
 #include <optional>
-#include <string_view>
+#include <stdexcept>
+#include <string>
 #include <utility>
 
-template <std::totally_ordered K, typename V, std::size_t N>
-class static_map {
-public:
-	using key_t = K;
-	using value_t = V;
-	using entry_t = std::pair<key_t, value_t>;
-	using map_t = std::array<entry_t, N>;
-	using it_t = typename map_t::const_iterator;
-	using find_t = std::optional<V>;
+#define STATIC_MAP_TYPEDEFS                      \
+public:                                          \
+	using key_t = K;                             \
+	using value_t = V;                           \
+	using entry_t = std::pair<key_t, value_t>;   \
+	using map_t = std::array<entry_t, N>;        \
+	using it_t = typename map_t::const_iterator; \
+	using find_t = std::optional<V>
 
+namespace detail {
+// Helper class that stores the actual data and acts differently depending on the properties of K
+template <typename K, typename V, std::size_t N>
+class storage_impl {};
+
+// K is totally ordered, which means we can sort! This sorts the map/keys on initialization and uses binary search to
+// find the values.
+template <std::totally_ordered K, typename V, std::size_t N>
+class storage_impl<K, V, N> {
+	STATIC_MAP_TYPEDEFS;
+
+private:
+	const map_t map;
+
+	static constexpr map_t sort_map(map_t unsorted_map) {
+		std::sort(unsorted_map.begin(), unsorted_map.end());
+
+		return unsorted_map;
+	}
+
+public:
+	constexpr explicit storage_impl(const map_t& map) : map{sort_map(map)} {}
+
+	[[nodiscard]] constexpr find_t find(key_t key) const {
+		const it_t begin = map.cbegin();
+		const it_t end = map.cend();
+
+		const it_t result =
+		    std::lower_bound(begin, end, key, [](const entry_t& a, const key_t& b) { return a.first < b; });
+
+		if ((result != end) && (result->first == key)) [[likely]] {
+			return std::make_optional(result->second);
+		} else {
+			return std::nullopt;
+		}
+	}
+};
+
+// K is just equality comparable, which means we can't sort the map/keys. This uses a linear search, which is the best
+// we can do for an unsorted list of data. This essentially acts as a fallback.
+template <std::equality_comparable K, typename V, std::size_t N>
+class storage_impl<K, V, N> {
+	STATIC_MAP_TYPEDEFS;
+
+private:
+	const map_t map;
+
+public:
+	constexpr explicit storage_impl(const map_t& map) : map{map} {}
+
+	[[nodiscard]] constexpr find_t find(const key_t& key) const {
+		const it_t begin = map.cbegin();
+		const it_t end = map.cend();
+
+		const it_t result = std::find_if(begin, end, [&key](const entry_t& a) { return a.first == key; });
+
+		if (result != end) [[likely]] {
+			return std::make_optional(result->second);
+		} else {
+			return std::nullopt;
+		}
+	}
+};
+}  // namespace detail
+
+template <std::equality_comparable K, typename V, std::size_t N>
+class static_map {
+	STATIC_MAP_TYPEDEFS;
+
+public:
 	// Mimic std::map's size functions
 	static constexpr std::size_t size() {
 		return N;
@@ -29,7 +99,7 @@ public:
 	};
 
 private:
-	const map_t sorted_map;
+	const detail::storage_impl<K, V, N> map;
 
 	static constexpr map_t sort_map(map_t unsorted_map) {
 		std::sort(unsorted_map.begin(), unsorted_map.end());
@@ -38,24 +108,14 @@ private:
 	}
 
 public:
-	constexpr explicit static_map(const map_t& map) : sorted_map{sort_map(map)} {}
+	constexpr explicit static_map(const map_t& map) : map{map} {}
 	constexpr explicit static_map(const entry_t (&&entries)[N]) : static_map{std::to_array(entries)} {}
 
-	[[nodiscard]] constexpr find_t find(key_t key) const {
-		const it_t begin = sorted_map.cbegin();
-		const it_t end = sorted_map.cend();
-
-		const it_t result =
-		    std::lower_bound(begin, end, key, [](const entry_t& a, const std::string_view& b) { return a.first < b; });
-
-		if ((result != end) && (result->first == key)) [[likely]] {
-			return std::make_optional(result->second);
-		} else {
-			return std::nullopt;
-		}
+	[[nodiscard]] constexpr find_t find(const key_t& key) const {
+		return map.find(key);
 	}
 
-	[[nodiscard]] constexpr value_t at(key_t key) const {
+	[[nodiscard]] constexpr value_t at(const key_t& key) const {
 		const find_t search_result = find(key);
 
 		if (search_result) [[likely]] {
@@ -68,11 +128,11 @@ public:
 		}
 	}
 
-	[[nodiscard]] constexpr bool contains(key_t key) const {
+	[[nodiscard]] constexpr bool contains(const key_t& key) const {
 		return find(key).has_value();
 	}
 
-	[[nodiscard]] constexpr value_t operator[](key_t key) const {
+	[[nodiscard]] constexpr value_t operator[](const key_t& key) const {
 		return at(key);
 	}
 };
